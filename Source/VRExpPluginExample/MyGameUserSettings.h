e92e86612d7508c3929de93c4f10ad083d0bfc0e
snonike <88960313+snonike@users.noreply.github.com>
2024-10-29 18:16:02 -0700

    Added full config

--- Source/VRExpPluginExample/MyGameUserSettings.h
+++ Source/VRExpPluginExample/MyGameUserSettings.h
@@ -50,14 +50,23 @@ public:
 protected:
 	UPROPERTY(Config)
 	bool ThighTracking;
+	UPROPERTY(Config)
 	bool KneeTracking;
+	UPROPERTY(Config)
 	bool FootTracking;
+	UPROPERTY(Config)
 	bool ChestTracking;
+	UPROPERTY(Config)
 	bool WaistTracking;
+	UPROPERTY(Config)
 	FString DefaultIPAddress;
+	UPROPERTY(Config)
 	bool IsUsingDefaultIPAddress;
+	UPROPERTY(Config)
 	bool DebugConfiguration;
+	UPROPERTY(Config)
 	bool PlayerCanSeeThemselves;
+	
 	
 
 	
